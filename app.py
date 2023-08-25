3ffc6eef83da92bc18115be10dd7d508c219accd
“KatherineZaRa” <katherine.zana@utec.edu.pe>
2023-08-25 15:12:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -71,20 +71,51 @@ animes = [
 
 ]
 
+@app.route('/')
+def index():
+    return '¡Hola, mundo! Esta es mi primera API con Flask.'
+
 # Método GET/anime
 @app.route('/anime',methods=['GET'])
 def get_anime():
     return jsonify(animes)
 # Método GET/anime/{id}
-
+@app.route('/anime/<id>',methods=['GET'])
+def get_id_anime(id):
+    for ids in animes:
+        if ids['id']==int(id):
+            return jsonify(ids)
+    return {}
 # Método POST/anime
-
+@app.route('/anime',methods=['POST'])
+def post_anime():
+    agregar = request.get_json()
+    agregar['id'] = len(animes) + 1
+    animes.append(agregar)
+    return jsonify(agregar)
 # Método PUT/anime/{id}
-
+@app.route('/anime/<id>',methods=['PUT'])
+def put_anime(id):
+    numero = request.get_json()
+    for i, u in enumerate(animes):
+        if u['id']== numero['id']:
+            animes[i]=numero
+    return jsonify(numero)
 # Método PATCH/anime/{id}
-
+#@app.route('/anime/<id>',methods=['PATCH'])
+#def patch_anime(id):
+#    numero = request.get_json()
+#    for i, u in enumerate(animes):
+#        if u['id']== numero['id']:
+#            animes[i]=numero
+#    return jsonify(numero)
 # Método DELETE/anime/{id}
-
+@app.route('/anime/<id>',methods=['DELETE'])
+def delete_anime(id):
+    for ids in animes:
+        if ids['id']==int(id):
+            animes.remove(ids)
+    return {}
 
 
 if __name__ == '__main__':
